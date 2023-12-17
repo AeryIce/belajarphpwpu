873f4e4c4e8505f8fc5f9a379e7320bcc690907d
AeryIce <agahariiswarajati@gmail.com>
2023-12-17 21:26:59 +0700

    Menambah Materi 1

--- routes/web.php
+++ routes/web.php
@@ -16,3 +16,7 @@ use Illuminate\Support\Facades\Route;
 Route::get('/', function () {
     return view('welcome');
 });
+Route::get('/materi1', function () {
+    return view('belajarphpwpu/materi1');
+});
+   
